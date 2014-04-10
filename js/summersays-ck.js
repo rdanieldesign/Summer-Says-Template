1d45e5619ac5220625ee2dc648cc8df8193668a3
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2014-04-09 21:46:35 -0400

    Add no-us backup for masonry
    Also, resize social

--- js/summersays-ck.js
+++ js/summersays-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".collapse-nav").click(function(){$(".expand").toggleClass("open")})});
+$(document).ready(function(){$(".collapse-nav").click(function(){$(".expand").toggleClass("open")});$(".post").addClass("js");$(".social-widget").addClass("js")});
--- js/summersays.js
+++ js/summersays.js
@@ -2,5 +2,7 @@ $(document).ready(function(){
 	$( ".collapse-nav" ).click(function() {
 		$( ".expand" ).toggleClass("open");
 	});
+	$(".post").addClass("js");
+	$(".social-widget").addClass("js");
 });
 
